f9a3e07190da28a24bbd0f7e745f38d9f77377f9
JoakimOrnerfors <Joakim.ornerfors@student.forsbergsskola.se>
2022-07-05 12:31:53 +0200

    Created New AI and Script
    Created Enemy AI '' GHOST '' and created a follow player script for it.

---
+++ Project-Sanity-GD21/Assets/Scripts/Enemy_Move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_Move : MonoBehaviour
+{
+    private NavMeshAgent Enemy;
+
+    private GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = GetComponent<NavMeshAgent>();
+        Player=GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Enemy.SetDestination(Player.transform.position);
+    }
+}
